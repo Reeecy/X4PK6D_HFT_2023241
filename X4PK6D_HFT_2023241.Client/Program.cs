439c535aad76c5b3555dbc235a16c3022340c21a
kocka <kocka@NACE>
2023-10-19 16:48:37 +0200

    Add project files.

---
+++ X4PK6D_HFT_2023241.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace X4PK6D_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ X4PK6D_HFT_2023241.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace X4PK6D_HFT_2023241.Models
+{
+    public class Class1
+    {
+    }
+}
