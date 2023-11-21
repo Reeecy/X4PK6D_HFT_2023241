d19477962b205a485aaaa7839af6446dedb15c8c
nace <tthignac@gmail.com>
2023-11-21 18:53:48 +0100

    logic layer done

--- X4PK6D_HFT_2023241.Client/Program.cs
+++ X4PK6D_HFT_2023241.Client/Program.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 using System.Linq;
+using X4PK6D_HFT_2023241.Logic;
 using X4PK6D_HFT_2023241.Models;
+using X4PK6D_HFT_2023241.Repository;
 
 namespace X4PK6D_HFT_2023241.Client
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            //IRepository<Person> repo = new PersonRepository(new FaMDbContext());
-            //var items = repo.Readall().ToArray();
+            // Repository and Logic references are added to the Client project
+            var ctx = new FaMDbContext();
+            var personRepo = new PersonRepository(ctx);
+            var passRepo = new PassRepository(ctx);
+            var entriesExitsRepo = new EntriesExitsRepository(ctx);
+
+            var personLogic = new PersonLogic(personRepo);
+            var passLogic = new PassLogic(passRepo);
+            var entriesExitsLogic = new EntriesExitsLogic(entriesExitsRepo);
+
 
         }
     }
