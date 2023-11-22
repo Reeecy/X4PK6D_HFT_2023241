5ee1c9314443c06424b554d44cd8fdab881ac4a6
nace <tthignac@gmail.com>
2023-11-22 19:03:00 +0100

    endpoint transients added

---
+++ X4PK6D_HFT_2023241.Endpoint/Controllers/PersonController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using X4PK6D_HFT_2023241.Logic;
+
+namespace X4PK6D_HFT_2023241.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        IPersonLogic logic;
+
+        public PersonController(IPersonLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- X4PK6D_HFT_2023241.Endpoint/Startup.cs
+++ X4PK6D_HFT_2023241.Endpoint/Startup.cs
@@ -1,21 +1,48 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X4PK6D_HFT_2023241.Logic;
+using X4PK6D_HFT_2023241.Models;
+using X4PK6D_HFT_2023241.Repository;
 
 namespace X4PK6D_HFT_2023241.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FaMDbContext>();
+
+            services.AddTransient<IRepository<Person>, PersonRepository>();
+            services.AddTransient<IRepository<Pass>,  PassRepository>();
+            services.AddTransient<IRepository<EntriesExits>, EntriesExitsRepository>();
+
+            services.AddTransient<IPersonLogic, PersonLogic>();
+            services.AddTransient<IPassLogic, PassLogic>();
+            services.AddTransient<IEntriesExitsLogic, EntriesExitsLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "X4PK6D_HFT_2023241.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +51,17 @@ namespace X4PK6D_HFT_2023241.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "X4PK6D_HFT_2023241.Endpoint v1"));
             }
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
