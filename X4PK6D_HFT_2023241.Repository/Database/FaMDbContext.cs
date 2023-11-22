a7abeb9a374321eb943687a27597dcc9a5acc8c5
nace <tthignac@gmail.com>
2023-11-22 21:24:50 +0100

    added some comments

--- X4PK6D_HFT_2023241.Repository/Database/FaMDbContext.cs
+++ X4PK6D_HFT_2023241.Repository/Database/FaMDbContext.cs
@@ -19,8 +19,8 @@ namespace X4PK6D_HFT_2023241.Repository
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseInMemoryDatabase("fandm");
-            //string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""|DataDirectory|\fandm.mdf"";Integrated Security=True;MultipleActiveResultSets=True";
-            //string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\Visual Studio Projects\X4PK6D_HFT_2023241\X4PK6D_HFT_2023241.Repository\fandm.mdf"";Integrated Security=True;MultipleActiveResultSets=True";
+            // to use cstring: string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""|DataDirectory|\fandm.mdf"";Integrated Security=True;MultipleActiveResultSets=True";
+            // to see tables: string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\Visual Studio Projects\X4PK6D_HFT_2023241\X4PK6D_HFT_2023241.Repository\fandm.mdf"";Integrated Security=True;MultipleActiveResultSets=True";
             //optionsBuilder.UseSqlServer(conn);
             optionsBuilder.UseLazyLoadingProxies();
             base.OnConfiguring(optionsBuilder);
