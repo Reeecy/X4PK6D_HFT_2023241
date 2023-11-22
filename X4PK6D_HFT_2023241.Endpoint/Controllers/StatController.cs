ebb98d580dfa9087422e9a5a908a9ee101203b79
nace <tthignac@gmail.com>
2023-11-22 21:00:29 +0100

    statcontroller class added

---
+++ X4PK6D_HFT_2023241.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using X4PK6D_HFT_2023241.Logic;
+
+namespace X4PK6D_HFT_2023241.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        IPersonLogic logic;
+
+        public StatController(IPersonLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public int? PersonCount()
+        {
+            return logic.PersonCount();
+        }
+    }
+}
