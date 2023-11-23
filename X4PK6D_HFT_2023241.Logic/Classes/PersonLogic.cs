9d70b65a5bb6386f7f028830a9ecaeece4343409
nace <tthignac@gmail.com>
2023-11-23 14:43:24 +0100

    PersonsWithoutPasses method fixed

--- X4PK6D_HFT_2023241.Logic/Classes/PersonLogic.cs
+++ X4PK6D_HFT_2023241.Logic/Classes/PersonLogic.cs
@@ -131,7 +131,7 @@ namespace X4PK6D_HFT_2023241.Logic
         {
             var persons = _repo.ReadAll();
             var personWithoutPasses = from x in persons
-                                      where x.Pass == null
+                                      where x.PassId < 1 || x.PassId > 10
                                       select new
                                       {
                                           FullName = $"{x.FirstName} {x.LastName}"
