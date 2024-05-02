15e60b59a911d7f8cb7533746374ba24abbfffea
nace <tthignac@gmail.com>
2024-05-02 13:46:38 +0200

    created and configured WpfClient

---
+++ X4PK6D_HFT_2023241.WPFClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace X4PK6D_HFT_2023241.WPFClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ X4PK6D_HFT_2023241.WPFClient/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using X4PK6D_HFT_2023241.Models;
+
+namespace X4PK6D_HFT_2023241.WPFClient
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        public RestCollection<Person> Persons { get; set; }
+
+        private Person selectedPerson;
+
+        public Person SelectedPerson
+        {
+            get { return selectedPerson; }
+            set { SetProperty(ref selectedPerson, value);
+                (DeletePersonCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+
+        public ICommand CreatePersonCommand { get; set; }
+
+        public ICommand DeletePersonCommand { get; set; }
+
+        public ICommand UpdatePersonCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Persons = new RestCollection<Person>("http://localhost:20677/", "person");
+            CreatePersonCommand = new RelayCommand(() =>
+            {
+                Persons.Add(new Person()
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Address = "ValamiVaros"
+                });
+            });
+
+            DeletePersonCommand = new RelayCommand(() =>
+            {
+                Persons.Delete(SelectedPerson.Id);
+            },
+            () =>
+            {
+                return SelectedPerson != null;
+            });
+        }
+    }
+}
